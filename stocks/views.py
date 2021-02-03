2d57fe1c585824e99505bf58cd7fa5822f3a2705
Raynier Portus <raynierportus.14@gmail.com>
2021-02-03 20:33:53 +0800

    Create API View

--- stocks/views.py
+++ stocks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework import permissions, status, generics
+from .serializers import *
+from .models import *
 # Create your views here.
+
+class StockCreateView(generics.CreateAPIView):
+    queryset = Stocks.objects.all()
+    serializer_class = StocksSerializer
